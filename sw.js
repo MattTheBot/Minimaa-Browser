705a3ee4031e2bac7e3f388cdc20dc4b5d9af1fe
MattTheBot <mattthonhauser@gmail.com>
2026-05-29 13:38:01 +0200

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,37 @@
+/* =========================
+OPTIONAL FILE: sw.js
+========================= */
+
+const CACHE_NAME = "minimaa-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then((response) => {
+
+        return response || fetch(event.request);
+      })
+  );
+});
